37c957953fa9560a94225e1fc1d7d1d8b64cb4c8
Mike Gillett <mgillett54@gmail.com>
2018-10-25 11:31:00 -0700

    Fixed bug with error logging and added memory info

--- post.js
+++ post.js
@@ -4,7 +4,9 @@ function exit() {}
 // takes a string as input and returns a string
 // like `echo <jsonstring> | jq <filter>`, returning the value of STDOUT
 function raw(jsonstring, filter, flags) {
-  if (!initialized) return "{}";
+  if (!initialized) {
+    return "{}";
+  }
 
   stdin = jsonstring;
   inBuffer = [];
@@ -36,10 +38,12 @@ function raw(jsonstring, filter, flags) {
 }
 
 // takes an object as input and tries to return objects.
-function json(json, filter) {
-  if (!initialized) return {};
+function json(inputJson, filter) {
+  if (!initialized) {
+    return {};
+  }
 
-  var jsonstring = JSON.stringify(json);
+  var jsonstring = JSON.stringify(inputJson);
   var result = raw(jsonstring, filter, ["-c"]).trim();
 
   if (result.indexOf("\n") !== -1) {
@@ -52,34 +56,35 @@ function json(json, filter) {
         })
         // Parse each line to json
         .map(function(line) {
-          return parseResult(line, "multi");
+          return parseResult(line, "multi", inputJson, filter);
         })
         // Filter any results that returned null from parseResult
-        .filter(function(json) {
-          return json;
+        .filter(function(parsedJson) {
+          return parsedJson;
         })
         // Merge each json line into an array
-        .reduce(function(acc, json) {
-          return acc.concat(json);
+        .reduce(function(acc, parsedJson) {
+          return acc.concat(parsedJson);
         }, [])
     );
   } else {
-    return parseResult(result, "single");
+    return parseResult(result, "single", inputJson, filter);
   }
 }
 
-function parseResult(jsonString, type) {
+function parseResult(jsonString, type, inputJson, filter) {
   try {
     return JSON.parse(jsonString);
   } catch (e) {
     var errorMsg = "JQ library error: " + e.message;
     window.Rollbar &&
       window.Rollbar.error(errorMsg, {
-        json: json,
+        json: inputJson,
         filter: filter,
         jqResultString: jsonString,
         stacktrace: e.stacktrace,
-        type: type
+        type: type,
+        memoryInfo: window.performance.memory
       });
     return null;
   }
